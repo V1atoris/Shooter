b82484a3438fe2ac335e4e84e74051aceb27cf2d
V1atoris <86653726+V1atoris@users.noreply.github.com>
2021-07-30 16:38:36 +0300

    Update ShooterCharacter.cpp

--- Source/Shooter/ShooterCharacter.cpp
+++ Source/Shooter/ShooterCharacter.cpp
@@ -242,59 +242,7 @@ bool AShooterCharacter::GetBeamEndLocation(const FVector& MuzzleSocketLocation, 
 	return false;
 
 
-	/*
-	//** Get current size of viewport 
-	FVector2D ViewportSize;
-	if (GEngine && GEngine->GameViewport)
-	{
-		GEngine->GameViewport->GetViewportSize(ViewportSize);
-	}
-
-
 	
-	//** Get screen space location of crosshairs
-	FVector2D CrosshairLocation(ViewportSize.X / 2.f, ViewportSize.Y / 2.f);
-	//***CrosshairLocation.Y -= 50.f;
-	FVector CrosshairWorldPosition;
-	FVector CrosshairWorldDirection;
-
-	//** Get world position and direction of crosshair
-	bool bScreenToWorld = UGameplayStatics::DeprojectScreenToWorld(UGameplayStatics::GetPlayerController(this, 0),
-		CrosshairLocation, CrosshairWorldPosition, CrosshairWorldDirection);
-	
-	if (bScreenToWorld) //*** Was deprojection successful ?
-	{
-		//** Set beam end line point to line trace end point
-		FHitResult ScreenTraceHit;
-		const FVector Start{ CrosshairWorldPosition };
-		const FVector End{ CrosshairWorldPosition + CrosshairWorldDirection * 50000.f };
-
-		//** Trace outward from crosshairs world location
-		OutBeamLocation = End;
-
-
-		GetWorld()->LineTraceSingleByChannel(ScreenTraceHit, Start, End, ECollisionChannel::ECC_Visibility);
-		if (ScreenTraceHit.bBlockingHit) //*** was there a trace hit ?
-		{
-			//** Beam end point is now trace hit location
-			OutBeamLocation = ScreenTraceHit.Location;
-
-
-			//*** Preform second trace, this time from gun barrel
-			FHitResult WeaponTraceHit;
-			const FVector WeaponTraceStart{ MuzzleSocketLocation };
-			const FVector WeaponTraceEnd{ OutBeamLocation };
-			GetWorld()->LineTraceSingleByChannel(WeaponTraceHit, WeaponTraceStart, WeaponTraceEnd, ECollisionChannel::ECC_Visibility);
-			if (WeaponTraceHit.bBlockingHit) //*** Object between barrel and BeamEndPoint ?
-			{
-				OutBeamLocation = WeaponTraceHit.Location;
-			}
-			return true;
-		}
-
-	}
-    return false;
-	*/
 }
 
 void AShooterCharacter::AimingButtonPressed()
